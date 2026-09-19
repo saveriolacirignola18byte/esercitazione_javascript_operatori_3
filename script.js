4ef4c9db109b528bdcf188dbafcbcd5b89247b47
Saverio Lacirignola <saveriolacirignola18@gmail.com>
2026-09-19 17:30:15 +0200

    Esercitazione Javascript Operatori 3

---
+++ script.js
@@ -0,0 +1,31 @@
+let num1 = parseInt(prompt("Inserisci il primo numero: "));//dichiarazione variabile num1, numero scelto dall'utente
+
+console.log(num1);
+
+let num2 = parseInt(prompt("Inserisci il secondo numero: "));//dichiarazione variabile num2, numero scelto dall'utente
+
+console.log(num2);
+
+let sum = num1 + num2;//dichiarazione variabile somma
+
+console.log(sum);
+
+let difference = num1 - num2;//dichiarazione variabile sottrazione
+
+console.log(difference);
+
+let product = num1 * num2;//dichiarazione variabile moltiplicazione
+
+console.log(product);
+
+let quotient = num1 / num2;//dichiarazione variabile divisione 
+
+console.log(quotient);
+
+let exponentiation = num1 ** num2;//dichiarazione variabile potenza
+
+console.log(exponentiation)
+
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono:`)
+
+console.log(`somma = ${sum}, sottrazione = ${difference}, moltiplicazione = ${product}, divisione = ${quotient}, potenza ${exponentiation}`)
